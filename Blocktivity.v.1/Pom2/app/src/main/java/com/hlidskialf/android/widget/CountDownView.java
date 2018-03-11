2e44d59da846d93eb09651670c51d860dc58b62a
Ben Johns <34700568+benjjii@users.noreply.github.com>
2018-03-10 23:01:58 -0500

    Final Project
    With use of Pomodoro Timer App techniques.

---
+++ Blocktivity.v.1/Pom2/app/src/main/java/com/hlidskialf/android/widget/CountDownView.java
@@ -0,0 +1,71 @@
+package com.hlidskialf.android.widget;
+
+import android.content.Context;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.widget.TextView;
+import java.lang.Runnable;
+import java.util.Locale;
+
+
+public class CountDownView extends TextView
+{
+  private long mUntil;
+  private Handler mHandler;
+  private Runnable mCallback;
+  private boolean mPaused,mRunning;
+
+  public CountDownView(Context context, AttributeSet attrs)
+  {
+    super(context,attrs);
+
+    mHandler = new Handler();
+    mCallback = new Runnable() { 
+      public void run() {
+        if (mRunning && !mPaused) {
+          CountDownView.this.tick();
+          mHandler.postDelayed(mCallback, 1000);
+        }
+      }
+    };
+  }
+
+  public void start(long durationMillis) { start(durationMillis,0); }
+  public void start(long durationMillis, long startTime)
+  {
+    startTime = (startTime == 0) ? System.currentTimeMillis() : startTime;
+    mUntil = startTime + durationMillis;
+
+    mPaused = false;
+    mRunning = true;
+    mHandler.postDelayed(mCallback, 0);
+  }
+
+  public void stop()
+  {
+    setText("00:00");
+    mRunning = false;
+  }
+  public void pause()
+  {
+    mPaused = true;
+  }
+  public void unpause()
+  {
+    mPaused = false;
+    mHandler.postDelayed(mCallback, 0);
+  }
+
+  protected void tick()
+  {
+    long left = mUntil - System.currentTimeMillis();
+    long min = Math.abs((long)(left / 60000));
+    long sec = Math.abs((long)((left - min) / 1000) % 60);
+    //setText((left < 0 ? "-" : "" )+ min+":"+sec);
+
+    String timeLeftFormatted = String.format(Locale.getDefault(),"%02d:%02d", min, sec);
+
+    setText((left < 0 ? "-" : "" )+ timeLeftFormatted);
+  }
+}
